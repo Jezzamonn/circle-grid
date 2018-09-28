9c7fd0aa4a50f14f8407771f7ad5f97c82bc5b65
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-09-27 19:56:53 -0700

    Well, it spins hahah

--- js/controller.js
+++ js/controller.js
@@ -1,20 +1,24 @@
-import { slurp } from "./util";
+import { slurp, easeInOut } from "./util";
 import { EHOSTUNREACH } from "constants";
 
 export default class Controller {
 
 	constructor() {
-
+		this.size = 200;
+		this.animAmt = 0;
+		this.period = 7;
 	}
 
 	update(dt) {
-		// TODO: Some updating logic
+		this.animAmt += dt / this.period;
+		this.animAmt %= 1;
 	}
 
 	/**
 	 * @param {CanvasRenderingContext2D} context 
 	 */
 	render(context) {
+		context.beginPath();
 		const numSquares = 8;
 		const size = 200;
 		for (let iy = 0; iy < numSquares; iy ++) {
@@ -28,45 +32,48 @@ export default class Controller {
 					continue;
 				}
 
-				context.beginPath();
-				context.fillStyle = 'black';
-				adjustedMoveTo(context, minY, minX);
-				adjustedLineTo(context, minY, maxX);
-				adjustedLineTo(context, maxY, maxX);
-				adjustedLineTo(context, maxY, minX);
-				context.closePath();
-				context.fill();
+				this.adjustedMoveTo(context, minY, minX);
+				this.adjustedLineTo(context, minY, maxX);
+				this.adjustedLineTo(context, maxY, maxX);
+				this.adjustedLineTo(context, maxY, minX);
+				this.adjustedLineTo(context, minY, minX);
 			}
 		}
+		context.fillStyle = 'black';
+		context.fill('evenodd');
 	}
 
-}
-
-function adjustedMoveTo(context, x, y) {
-	let adjusted = adjustPoint({x: x, y: y});
-	context.moveTo(adjusted.x, adjusted.y);
-}
-
-function adjustedLineTo(context, x, y) {
-	let adjusted = adjustPoint({x: x, y: y});
-	context.lineTo(adjusted.x, adjusted.y);
-}
-
-// stolen from https://www.xarg.org/2017/07/how-to-map-a-square-to-a-circle/
-function adjustPoint(point) {
-	let r = Math.sqrt(point.x * point.x + point.y * point.y);
-	let theta = Math.atan2(point.y, point.x);
-
-	let normalisingFactor = 1;
-	if (Math.abs(point.x) > Math.abs(point.y)) {
-		normalisingFactor = Math.abs(Math.cos(theta));
+	adjustedMoveTo(context, x, y) {
+		let adjusted = this.adjustPoint({x: x, y: y});
+		context.moveTo(adjusted.x, adjusted.y);
 	}
-	else {
-		normalisingFactor = Math.abs(Math.sin(theta));
+	
+	adjustedLineTo(context, x, y) {
+		let adjusted = this.adjustPoint({x: x, y: y});
+		context.lineTo(adjusted.x, adjusted.y);
 	}
-
-	return {
-		x: normalisingFactor * r * Math.cos(theta),
-		y: normalisingFactor * r * Math.sin(theta),
+	
+	// stolen from https://www.xarg.org/2017/07/how-to-map-a-square-to-a-circle/
+	adjustPoint(point) {
+		let r = Math.sqrt(point.x * point.x + point.y * point.y);
+		let theta = Math.atan2(point.y, point.x);
+	
+		let normalisingFactor = 1;
+		if (Math.abs(point.x) > Math.abs(point.y)) {
+			normalisingFactor = Math.abs(Math.cos(theta));
+		}
+		else {
+			normalisingFactor = Math.abs(Math.sin(theta));
+		}
+		// Just normalise here
+		r *= normalisingFactor;
+		let rAmt = r / this.size;
+		let spinAmt = slurp(-1, 1, rAmt) * easeInOut(this.animAmt);
+		let spinAngle = 4 * Math.PI * spinAmt;
+	
+		return {
+			x: r * Math.cos(theta + spinAngle),
+			y: r * Math.sin(theta + spinAngle),
+		}
 	}
 }
