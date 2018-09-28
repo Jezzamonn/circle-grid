dcb0526ba663a054efc945298fe33b0fe4706e95
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-09-27 19:19:11 -0700

    Got a basic grid rendering... ok...

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,5 @@
+import { slurp } from "./util";
+
 export default class Controller {
 
 	constructor() {
@@ -8,8 +10,28 @@ export default class Controller {
 		// TODO: Some updating logic
 	}
 
+	/**
+	 * @param {CanvasRenderingContext2D} context 
+	 */
 	render(context) {
-		// TODO: Some rendering logic
+		const numSquares = 8;
+		const size = 200;
+		context.fillStyle = 'black';
+		for (let iy = 0; iy < numSquares; iy ++) {
+			const minY = slurp(-size, size, iy / numSquares);
+			const maxY = slurp(-size, size, (iy + 1) / numSquares);
+			for (let ix = 0; ix < numSquares; ix ++) {
+				const minX = slurp(-size, size, ix / numSquares);
+				const maxX = slurp(-size, size, (ix + 1) / numSquares);
+
+				if ((ix + iy) % 2 == 0) {
+					continue;
+				}
+
+				context.rect(minX, minY, maxX - minX, maxY - minY);
+			}
+		}
+		context.fill();
 	}
 
 }
